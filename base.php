ba264fd073ddf0bfa5310464cd4bfdc9536e92b6
root <root@lvps62-112-145-48.my-simplyroot.de>
2014-02-15 17:28:23 +0100

    changed page layout

--- base.php
+++ base.php
@@ -22,7 +22,7 @@
       <main class="main <?php echo roots_main_class(); ?>" role="main">
         <?php include roots_template_path(); ?>
       </main><!-- /.main -->
-      <?php if (roots_display_sidebar()) : ?>
+      <?php if (roots_display_sidebar() && false) : ?>
         <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
           <?php include roots_sidebar_path(); ?>
         </aside><!-- /.sidebar -->
